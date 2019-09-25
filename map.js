981c1e0a32a9c5bc9dac6e2c4d523e5be3dbfa46
Esbern Holmes <holmes@ruc.dk>
2019-09-25 22:50:51 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,16 @@
+ /*  This component can be called with the following properties
+*    lat: 55.65321   The map center latitude
+*    lng: 12.1412    The map center longitude
+*    zoom: 17        The map zoom level
+*    mapHight : 600  The height of the map div
+*    geoJSON: {"type": "FeatureCollection", "features": []}   The geoJSON to display on the map
+*   The component is designed for use in Gatsby.js 
+*/
+
 import React from "react"
-import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
-
-
-var defaultGeoJSON = {"type": "FeatureCollection", "features": []};
+import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 
 class MyMap extends React.Component {
-
-
   render() {
     const position = [this.props.lat, this.props.lng]
     return (
@@ -15,7 +19,7 @@ class MyMap extends React.Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={this.props.GeoJSON} />
+        <GeoJSON data={this.props.geoJSON} />
 
       </Map>
     )
@@ -25,7 +29,7 @@ MyMap.defaultProps = {
     lat: 55.65321,
     lng: 12.1412,
     zoom: 17,
-    mapHight : 600
-    GeoJSON: defaultGeoJSON
+    mapHight : 600,
+    geoJSON: {"type": "FeatureCollection", "features": []}
   }
 export default MyMap
